d363feeae5b8d7c39d5340b8da0b20f79aac2fdb
lud0914 <77470657+lud0914@users.noreply.github.com>
2026-01-19 08:57:12 +0000

    fixed keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,15 @@ import tailwind from "@astrojs/tailwind";
 import keystatic from "@keystatic/astro"; // 1. Keystatic 임포트 추가
 import { SITE_URL } from "./src/consts";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
   base: '/',
-  output: 'hybrid', // 2. 관리자 UI API 동작을 위해 'hybrid' 설정 필수 추가
-  integrations: [
-    mdx(), 
-    sitemap(), 
-    tailwind(), 
-    keystatic() // 3. Keystatic 인티그레이션 추가
-  ],
+  output: 'static', // 2. 관리자 UI API 동작을 위해 'hybrid' 설정 필수 추가
+  integrations: [mdx(), sitemap(), tailwind(), // 3. Keystatic 인티그레이션 추가
+  keystatic(), react()],
   markdown: {
     shikiConfig: {
       themes: {
