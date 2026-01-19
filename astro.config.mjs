16bd9ec5da713581a53df1254ae6f1ee21a3877d
lud0914 <77470657+lud0914@users.noreply.github.com>
2026-01-19 08:48:26 +0000

    Add Keystatic Admin Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import keystatic from "@keystatic/astro"; // 1. Keystatic 임포트 추가
 import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
   base: '/',
-  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'hybrid', // 2. 관리자 UI API 동작을 위해 'hybrid' 설정 필수 추가
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    tailwind(), 
+    keystatic() // 3. Keystatic 인티그레이션 추가
+  ],
   markdown: {
     shikiConfig: {
       themes: {
